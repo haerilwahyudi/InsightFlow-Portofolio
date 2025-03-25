cded628cc78444cd6a93c29b7dc68da64a197b6b
haerilwahyudi <haerilwahyudi12@gmail.com>
2025-03-25 16:04:09 +0700

    Update rating_predictor.py

--- product_analytics/models/predictive_models/rating_predictor.py
+++ product_analytics/models/predictive_models/rating_predictor.py
@@ -1,1 +1,48 @@
  
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import GridSearchCV
+import xgboost as xgb
+import lightgbm as lgb
+
+class AdvancedRatingPredictor:
+    def __init__(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.models = {
+            'xgboost': xgb.XGBRegressor(),
+            'lightgbm': lgb.LGBMRegressor(),
+            'gboost': GradientBoostingRegressor()
+        }
+        
+    def hyperparameter_tuning(self):
+        """Grid search for optimal parameters"""
+        param_grid = {
+            'xgboost': {
+                'n_estimators': [100, 200],
+                'max_depth': [3, 5],
+                'learning_rate': [0.01, 0.1]
+            },
+            # Add other model params...
+        }
+        
+        self.best_models = {}
+        for name, model in self.models.items():
+            gs = GridSearchCV(
+                model,
+                param_grid[name],
+                cv=5,
+                scoring='neg_mean_squared_error'
+            )
+            gs.fit(self.X_train, self.y_train)
+            self.best_models[name] = gs.best_estimator_
+            
+        return self.best_models
+    
+    def ensemble_prediction(self, X_test):
+        """Combine predictions from multiple models"""
+        predictions = []
+        for name, model in self.best_models.items():
+            pred = model.predict(X_test)
+            predictions.append(pred)
+            
+        return np.mean(predictions, axis=0)
