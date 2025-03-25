efede54266fe5bfc2e8af93a433c4cdd55d48479
haerilwahyudi <haerilwahyudi12@gmail.com>
2025-03-25 16:15:58 +0700

    Update rating_predictor.py

--- product_analytics/models/predictive_models/rating_predictor.py
+++ product_analytics/models/predictive_models/rating_predictor.py
@@ -1,48 +1,73 @@
- 
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.model_selection import GridSearchCV
+import optuna
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import StackingRegressor
+from sklearn.linear_model import RidgeCV
 import xgboost as xgb
 import lightgbm as lgb
+import catboost as cb
 
-class AdvancedRatingPredictor:
-    def __init__(self, X_train, y_train):
-        self.X_train = X_train
-        self.y_train = y_train
-        self.models = {
-            'xgboost': xgb.XGBRegressor(),
-            'lightgbm': lgb.LGBMRegressor(),
-            'gboost': GradientBoostingRegressor()
-        }
-        
-    def hyperparameter_tuning(self):
-        """Grid search for optimal parameters"""
-        param_grid = {
-            'xgboost': {
-                'n_estimators': [100, 200],
-                'max_depth': [3, 5],
-                'learning_rate': [0.01, 0.1]
-            },
-            # Add other model params...
-        }
-        
-        self.best_models = {}
-        for name, model in self.models.items():
-            gs = GridSearchCV(
-                model,
-                param_grid[name],
-                cv=5,
-                scoring='neg_mean_squared_error'
-            )
-            gs.fit(self.X_train, self.y_train)
-            self.best_models[name] = gs.best_estimator_
-            
-        return self.best_models
+class RatingPredictorOptimizer:
+    """Advanced rating prediction with hyperparameter optimization"""
     
-    def ensemble_prediction(self, X_test):
-        """Combine predictions from multiple models"""
-        predictions = []
-        for name, model in self.best_models.items():
-            pred = model.predict(X_test)
-            predictions.append(pred)
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+        self.study = None
+        self.best_model = None
+        
+    def optimize_xgb(self, n_trials=100):
+        """Bayesian optimization for XGBoost"""
+        def objective(trial):
+            params = {
+                'n_estimators': trial.suggest_int('n_estimators', 100, 1000),
+                'max_depth': trial.suggest_int('max_depth', 3, 10),
+                'learning_rate': trial.suggest_loguniform('learning_rate', 0.01, 0.3),
+                'subsample': trial.suggest_uniform('subsample', 0.6, 1.0),
+                'colsample_bytree': trial.suggest_uniform('colsample_bytree', 0.6, 1.0),
+                'gamma': trial.suggest_loguniform('gamma', 1e-8, 1.0),
+                'reg_alpha': trial.suggest_loguniform('reg_alpha', 1e-8, 1.0),
+                'reg_lambda': trial.suggest_loguniform('reg_lambda', 1e-8, 1.0)
+            }
             
-        return np.mean(predictions, axis=0)
+            model = xgb.XGBRegressor(**params)
+            return cross_val_score(
+                model, self.X, self.y,
+                scoring='neg_mean_squared_error',
+                cv=5
+            ).mean()
+            
+        self.study = optuna.create_study(direction='maximize')
+        self.study.optimize(objective, n_trials=n_trials)
+        
+        self.best_model = xgb.XGBRegressor(**self.study.best_params)
+        self.best_model.fit(self.X, self.y)
+        return self.best_model
+        
+    def create_ensemble(self):
+        """Stacking ensemble of multiple models"""
+        estimators = [
+            ('xgb', xgb.XGBRegressor()),
+            ('lgb', lgb.LGBMRegressor()),
+            ('cat', cb.CatBoostRegressor(verbose=0))
+        ]
+        
+        self.ensemble = StackingRegressor(
+            estimators=estimators,
+            final_estimator=RidgeCV(),
+            cv=5
+        )
+        self.ensemble.fit(self.X, self.y)
+        return self.ensemble
+        
+    def explain_model(self, model=None):
+        """SHAP explanations for model interpretability"""
+        import shap
+        if model is None:
+            model = self.best_model
+            
+        explainer = shap.Explainer(model)
+        shap_values = explainer(self.X)
+        
+        # Visualization
+        shap.summary_plot(shap_values, self.X)
+        return shap_values
